a2bdd201adf2a6c90fdfd7b94287bc5300577143
maciejsyp <maciej.sypula@gmail.com>
2018-08-30 09:20:08 +0200

    changed switch to if statement, dealed with wrong menu number selection

---
+++ src/RunningCalculator/Menu.java
@@ -0,0 +1,20 @@
+package RunningCalculator;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public static int menu(){
+        System.out.println("Wybierz dystans, dla którego chcesz obliczyć szacowane tempo. Wybierz liczbę i potwierdź " +
+                "Enterem:");
+        System.out.println("  1. 5 kilometrów");
+        System.out.println("  2. 10 kilometrów");
+        System.out.println("  3. Półmaraton");
+        System.out.println("  4. Maraton");
+
+        Scanner in = new Scanner(System.in);
+        int choice = in.nextInt();
+
+        return choice;
+    }
+}
