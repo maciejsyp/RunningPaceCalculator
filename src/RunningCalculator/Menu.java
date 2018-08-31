aee793699ba85b0efac02b8cdfab3c1fe19cef5e
maciejsyp <maciej.sypula@gmail.com>
2018-08-31 22:41:33 +0200

    deleted unused imports

--- src/RunningCalculator/Menu.java
+++ src/RunningCalculator/Menu.java
@@ -1,16 +1,5 @@
 package RunningCalculator;
 
-import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 import java.util.Scanner;
 
 public class Menu {
